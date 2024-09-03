abcf49bef823348b5ba0a9ebc62aad76be74d844
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-03 11:33:33 +0200

    add daisy ui

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["../**/*.{html,js,php}"],
+	content: ["../**/*.{html,php}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('daisyui'),
+	],
 }
